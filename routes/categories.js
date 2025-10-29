b07fe5edf15ac4bac09fd1d91dce8e83657f55a3
Ron <ronshadreck@gmail.com>
2025-10-29 11:22:32 +0200

    added feature that deletes associated records when a category is deleted

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Category = require('../models/Category');
 const router = express.Router();
+const Record = require('../models/Record')
 
 router.get('/', async (req, res) => {
     let categories = await Category.findAll();
@@ -38,8 +39,20 @@ router.delete('/:id', async (req, res) => {
     let category = await Category.findOne({where: {id: req.params.id}});
     if (!category) return res.status(404).json({messege: 'Category not found'})
 
+    const categoryRecords = await Record.findAll({
+        where: {
+            categoryId: category.id
+        }
+    })
+
+    if (categoryRecords.length > 0) {
+        for (const record of categoryRecords) {
+            await record.destroy()
+        }
+    }
+
     await category.destroy();
     res.json({message: {message: 'Category deleted successfully'}})
-})
+});
 
 module.exports = router
