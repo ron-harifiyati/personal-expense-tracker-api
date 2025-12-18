ad72b2a3c780faf0daeddda1afc753a36278f58e
Ron <ronshadreck@gmail.com>
2025-12-19 01:39:54 +0200

    Refactored routes and added comments

--- routes/categories.js
+++ routes/categories.js
@@ -3,56 +3,88 @@ const Category = require('../models/Category');
 const router = express.Router();
 const Record = require('../models/Record')
 
+// Get all categories
 router.get('/', async (req, res) => {
     let categories = await Category.findAll();
     res.json(categories)
 });
 
+// Get all expense categories
+router.get('/expense', async (req, res) => {
+    let categories = await Category.findAll({ where: { type: 'expense' } });
+    res.json(categories)
+});
+
+// Get all income categories
+router.get('/income', async (req, res) => {
+    let categories = await Category.findAll({ where: { type: 'income' } });
+    res.json(categories)
+});
+
+// Create a new category
 router.post('/', async (req, res) => {
     const { title, type, imageTitle } = req.body;
-    if (!title) return res.status(400).json({message: 'Title is required'});
+    if (!title) return res.status(400).json({ message: 'Title is required' });
+    if (!type) return res.status(400).json({ message: 'Type is required' });
 
-    let category = await Category.create({
-        title,
-        type,
-        imageTitle
-    });
+    try {
+        let category = await Category.create({
+            title,
+            type,
+            imageTitle
+        });
 
-    res.status(201).json({category})
+        res.status(201).json({ message: "Category created successfully" })
+    } catch (err) {
+        req.status(500).json({ message: "Unexpexted server error" })
+    }
 });
 
+// Modify an existing category
 router.patch('/:id', async (req, res) => {
     const { title, type, imageTitle } = req.body;
-    let category = await Category.findOne({where: {id: req.params.id}});
+    if (!title) return res.status(400).json({ message: 'Title is required' });
+    if (!type) return res.status(400).json({ message: 'Type is required' });
 
-    if (!category) return res.status(400).json({message: 'Category not found'});
+    try {
+        let category = await Category.findOne({ where: { id: req.params.id } });
 
-    category.title = title ?? category.title;
-    category.type = type ?? category.type;
-    category.imageName = imageTitle ?? category.imageTitle;
-    await category.save();
+        if (!category) return res.status(400).json({ message: 'Category not found' });
 
-    res.json({category})
+        category.title = title ?? category.title;
+        category.type = type ?? category.type;
+        category.imageName = imageTitle ?? category.imageTitle;
+        await category.save();
+
+        res.status(200).json({ messege: "Category modified successfully" })
+    } catch (err) {
+        req.status(500).json({ message: "Unexpexted server error" })
+    }
 });
 
+// Delete an existing category
 router.delete('/:id', async (req, res) => {
-    let category = await Category.findOne({where: {id: req.params.id}});
-    if (!category) return res.status(404).json({messege: 'Category not found'})
+    try {
+        let category = await Category.findOne({ where: { id: req.params.id } });
+        if (!category) return res.status(404).json({ messege: 'Category not found' })
 
-    const categoryRecords = await Record.findAll({
-        where: {
-            categoryId: category.id
-        }
-    })
+        const categoryRecords = await Record.findAll({
+            where: {
+                categoryId: category.id
+            }
+        })
 
-    if (categoryRecords.length > 0) {
-        for (const record of categoryRecords) {
-            await record.destroy()
+        if (categoryRecords.length > 0) {
+            for (const record of categoryRecords) {
+                await record.destroy()
+            }
         }
+
+        await category.destroy();
+        res.status(200).json({ message: { message: 'Category deleted successfully' } })
+    } catch (err) {
+        req.status(500).json({ message: "Unexpexted server error" })
     }
-
-    await category.destroy();
-    res.json({message: {message: 'Category deleted successfully'}})
 });
 
 module.exports = router
--- routes/records.js
+++ routes/records.js
@@ -5,13 +5,35 @@ const Account = require('../models/Account');
 const Category = require('../models/Category');
 const Record = require('../models/Record');
 
+// Get all records
 router.get('/', async (req, res) => {
     const records = await Record.findAll();
     res.json(records)
 });
 
+// Get transfer records
+router.get('/transfer', async (req, res) => {
+    const records = await Record.findAll({ where: { type: "transfer" } });
+    res.json(records)
+});
+
+// Get income records
+router.get('/income', async (req, res) => {
+    const records = await Record.findAll({ where: { type: "income" } });
+    res.json(records)
+});
+
+// Get expense records
+router.get('/expense', async (req, res) => {
+    const records = await Record.findAll({ where: { type: "expense" } });
+    res.json(records)
+});
+
+// Create a new record
 router.post('/', async (req, res) => {
     const { type, amountString, fromAccountId, toAccountId, categoryId, notes } = req.body;
+    if (!type) return res.status(400).json({ message: "Type is required" });
+    if (!amountString) return res.status(400).json({ message: "Amount is required" });
 
     try {
         let result;
@@ -20,26 +42,24 @@ router.post('/', async (req, res) => {
         const fromAccount = await Account.findByPk(fromAccountId);
         const toAccount = await Account.findByPk(toAccountId);
 
-        if (!amountString) return res.status(400).json({ success: false, message: 'Amount is required.' });
-
         switch (type.toLowerCase()) {
             case 'expense':
-                if (!fromAccount || !category) return res.status(400).json({ success: false, message: 'Source Account and Category required for Expense.' });
+                if (!fromAccount || !category) return res.status(400).json({ message: 'Source Account and Category required for Expense.' });
                 result = await TransactionService.createExpense({ fromAccount, category, amountString, notes });
                 break;
 
             case 'income':
-                if (!toAccount || !category) return res.status(400).json({ success: false, message: 'Destination Account and Category required for Income.' });
+                if (!toAccount || !category) return res.status(400).json({ message: 'Destination Account and Category required for Income.' });
                 result = await TransactionService.createIncome({ toAccount, category, amountString, notes });
                 break;
 
             case 'transfer':
-                if (!fromAccount || !toAccount) return res.status(400).json({ success: false, message: 'Source and Destination Accounts required for Transfer.' });
+                if (!fromAccount || !toAccount) return res.status(400).json({ message: 'Source and Destination Accounts required for Transfer.' });
                 result = await TransactionService.createTransfer({ fromAccount, toAccount, amountString, notes });
                 break;
 
             default:
-                return res.status(400).json({ success: false, message: 'Invalid transaction type.' });
+                return res.status(400).json({ message: "Invalid transaction type." });
         }
 
         if (result.success) {
@@ -50,78 +70,76 @@ router.post('/', async (req, res) => {
         }
 
     } catch (error) {
-        res.status(500).json({ success: false, message: 'An unexpected server error occurred.' });
+        res.status(500).json({ message: 'An unexpected server error occurred.' });
     }
 })
 
+// Update an existinf=g record
 router.patch('/:id', async (req, res) => {
     const { type, amountString, fromAccountId, toAccountId, categoryId, notes } = req.body;
     const id = req.params.id
 
+    if (!type) return res.status(400).json({ message: "Type is required" });
+    if (!amountString) return res.status(400).json({ message: "Amount is required" });
+    if (!amountString) return res.status(400).json({ message: 'Amount is required.' });
 
     try {
         let result;
 
         const existingRecord = await Record.findByPk(id);
-        if (!existingRecord) return res.status(404).json({ success: false, message: 'Record not found' });
+        if (!existingRecord) return res.status(404).json({ message: 'Record not found' });
 
-        await TransactionService.undo(id)
+        await TransactionService.reverseTransaction(id)
 
         const category = await Category.findByPk(categoryId);
         const fromAccount = await Account.findByPk(fromAccountId);
         const toAccount = await Account.findByPk(toAccountId);
 
-        if (!amountString) return res.status(400).json({ success: false, message: 'Amount is required.' });
-
         switch (type.toLowerCase()) {
             case 'expense':
-                if (!fromAccount || !category) return res.status(400).json({ success: false, message: 'Source Account and Category required for Expense.' });
+                if (!fromAccount || !category) return res.status(400).json({ message: 'Source Account and Category required for Expense.' });
                 result = await TransactionService.createExpense({ id, fromAccount, category, amountString, notes });
                 break;
 
             case 'income':
-                if (!toAccount || !category) return res.status(400).json({ success: false, message: 'Destination Account and Category required for Income.' });
+                if (!toAccount || !category) return res.status(400).json({ message: 'Destination Account and Category required for Income.' });
                 result = await TransactionService.createIncome({ id, toAccount, category, amountString, notes });
                 break;
 
             case 'transfer':
-                if (!fromAccount || !toAccount) return res.status(400).json({ success: false, message: 'Source and Destination Accounts required for Transfer.' });
+                if (!fromAccount || !toAccount) return res.status(400).json({ message: 'Source and Destination Accounts required for Transfer.' });
                 result = await TransactionService.createTransfer({ id, fromAccount, toAccount, amountString, notes });
                 break;
 
             default:
-                return res.status(400).json({ success: false, message: 'Invalid transaction type.' });
+                return res.status(400).json({ message: 'Invalid transaction type.' });
         }
 
         if (result.success) {
-            res.json({ success: true, message: 'Successfully edited record' });
+            res.json({ message: 'Successfully edited record' });
         } else {
             // This handles insufficient funds, validation errors, etc., thrown by the service
             res.status(400).json(result);
         }
-
     } catch (error) {
-        console.log(error)
-        res.status(500).json({ success: false, message: 'An unexpected server error occurred.' });
+        res.status(500).json({ message: 'An unexpected server error occurred.' });
     }
 })
 
+// Delete an exising account
 router.delete('/:id', async (req, res) => {
     const id = req.params.id
 
     try {
-        const result = await TransactionService.delete(id)
+        const result = await TransactionService.reverseTransaction(id)
 
         if (result.success) {
             res.json({ success: true, message: 'Successfully deleted record' });
         } else {
             res.status(400).json(result);
         }
-
     } catch (error) {
-
-            console.log(error)
-        res.status(500).json({ success: false, message: 'An unexpected server error occurred.' });
+        res.status(500).json({ message: 'An unexpected server error occurred.' });
     }
 })
 
